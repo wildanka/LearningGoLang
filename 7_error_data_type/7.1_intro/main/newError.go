6adaf7dd52d7a68d407d7bc6f3bdac2bd5ed91a8
WildanKurniadi <danprogramming10@gmail.com>
2019-09-01 18:42:40 +0700

    #1.7.1.a converting error to strign

--- 7_error_data_type/7.1_intro/main/newError.go
+++ 7_error_data_type/7.1_intro/main/newError.go
@@ -32,5 +32,21 @@ func main()  {
 	if err.Error() == "Error in returnError() function!"{ // lihat line 10
 		fmt.Println("!!")
 	}
+	/* 
+		OUTPUT : 
+		$ go run newError.go
+		returnError() ended normally! (which means no error returned, it returns nil instead
+		Error in returnError() function!
+		!!
 
+		jika kita tidak mencoba langsung membandngkan antara [err] dengan string "Error in returnError() function!" (contoh kode sebagai berikut) : 
+		if err == "Error in returnError() function!"{ // lihat line 10
+			fmt.Println("!!")
+		}
+		
+		maka akan terjadi error seperti berikut: 
+		$ go run newError.go
+		# command-line-arguments
+		.\newError.go:32:9: invalid operation: err == "Error in returnError() function!" (mismatched types error and string)
+	*/
 }
